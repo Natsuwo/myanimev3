1e6bcbe890ea1a50ea910743922d9dada631b5a2
kirigayakazuto42@gmail.com <kirigayakazuto42@gmail.com>
2020-03-13 15:31:30 +0700

    fix port

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const app = require('./app')(__dirname)
 const server = require('http').Server(app)
+const port = process.env.PORT || 4000
 // const io = require('socket.io')(server)
 // require('./socketio')(io)
-server.listen(4000);
+server.listen(port);
